805df7e5c8076e5f8a02af1ea9410bb3c090e3c1
Sharon <s.klinkenberg@uva.nl>
2018-09-04 11:04:03 +0200

    added function to create multiple outputs from one rmd file

---
+++ render_course.R
@@ -0,0 +1,29 @@
+# Render course rmd
+
+# NOT RUN {
+library(rmarkdown)
+library(stringr)
+
+RenderMultipleOutput <- function(path) {
+
+file = str_extract(path, "\\w+.Rmd$")
+  
+# render the default (first) format defined in the file
+rmarkdown::render(input         = path, 
+                  output_format = "ioslides_presentation",
+                  output_file   = gsub("....$", "_IOslides.html", file)
+                  )
+
+rmarkdown::render(input         = path, 
+                  output_format = "html_document",
+                  output_file   = gsub("....$", "_handout.html", file)
+)
+
+}
+
+RenderMultipleOutput("courses/current/reasoning_nhst/reasoning_empirical_cycle_nhst.Rmd")
+RenderMultipleOutput("courses/current/coin_experiment/coin_experiment.Rmd")
+
+
+
+
