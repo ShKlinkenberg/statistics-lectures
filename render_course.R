3fb7ed0e559ae8be524cc7fb10b6efbd33056f27
Sharon <s.klinkenberg@uva.nl>
2018-10-09 13:32:46 +0200

    new lecture added

--- render_course.R
+++ render_course.R
@@ -15,13 +15,17 @@ RenderMultipleOutput <- function(path) {
                     output_format = "ioslides_presentation",
                     output_file   = gsub("....$", "_IOslides.html", file)
                     )
-  
+
+  file = str_extract(path, "\\w+.Rmd$")
+      
   rmarkdown::render(input         = path, 
                     output_format = "html_document",
                     output_file   = gsub("....$", "_handout.html", file)
                     )
 
 }
+
+# Blok I
 
 # Week 1
 RenderMultipleOutput("courses/current/reasoning_nhst/reasoning_empirical_cycle_nhst.Rmd")
@@ -38,3 +42,12 @@ RenderMultipleOutput("courses/current/t_distribution_and_one_sample_t_test/t_dis
 RenderMultipleOutput("courses/current/paired_and_independent_t_test/paired_and_independent_t_test.Rmd")
 RenderMultipleOutput("courses/current/the_beast_of_bias/the_beast_of_bias.Rmd")
 
+# Blok II
+
+# Week 6
+
+RenderMultipleOutput("courses/current/chi_squared_test/chi_squared_test.Rmd")
+
+# Week 7
+
+# RenderMultipleOutput("")
