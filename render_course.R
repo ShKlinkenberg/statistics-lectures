e6e7b5c254cabafac3eac7a50421a3f99421c892
Sharon <s.klinkenberg@uva.nl>
2018-10-10 14:00:23 +0200

    anova lecture added changes made to render function

--- render_course.R
+++ render_course.R
@@ -6,23 +6,26 @@ rm(list = ls())
 library(rmarkdown)
 library(stringr)
 
-RenderMultipleOutput <- function(path) {
-
+RenderOutput <- function(path, format_type = 1) {
+  
+  if(format_type == 1) { format = "html_document";         suffix = "_handout.html"  }
+  if(format_type == 2) { format = "ioslides_presentation"; suffix = "_IOslides.html" }
+  
   file = str_extract(path, "\\w+.Rmd$")
     
-  # render the default (first) format defined in the file
+  # render the default (first) format defined in the file "html_document"
+  
   rmarkdown::render(input         = path, 
-                    output_format = "ioslides_presentation",
-                    output_file   = gsub("....$", "_IOslides.html", file)
-                    )
+                    output_format = format,
+                    output_file   = gsub("....$", suffix, file)
+  ) 
+}
 
-  file = str_extract(path, "\\w+.Rmd$")
-      
-  rmarkdown::render(input         = path, 
-                    output_format = "html_document",
-                    output_file   = gsub("....$", "_handout.html", file)
-                    )
-
+RenderMultipleOutput <- function(path) {
+  
+  RenderOutput(path, 1)
+  RenderOutput(path, 2)
+  
 }
 
 # Blok I
@@ -47,6 +50,7 @@ RenderMultipleOutput("courses/current/the_beast_of_bias/the_beast_of_bias.Rmd")
 # Week 6
 
 RenderMultipleOutput("courses/current/chi_squared_test/chi_squared_test.Rmd")
+RenderMultipleOutput("courses/current/f_distribution_ANOVA_one_way_independent/f_distribution_ANOVA_one_way_independent.Rmd")
 
 # Week 7
 
